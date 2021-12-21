d36b2efcd924190a837901c014236da20850937a
hisocean <jobcho6320@naver.com>
2021-12-21 22:48:27 +0900

    refresh

--- codeit.py
+++ codeit.py
@@ -6,3 +6,15 @@ age = 32
 
 # 새로운 f-string 방식
 print(f"제 이름은 {name}이고 {age}살입니다.")
+
+
+# 문자열 포맷팅 실습과제
+
+wage = 5
+korean_wage = 5710.8
+exchange_rate = korean_wage/wage
+
+print(f"{1}시간에 {wage}달러 벌었습니다.")
+print(f"{5}시간에 {wage*5}달러 벌었습니다.")
+print(f"{1}시간에 {korean_wage}달러 벌었습니다.")
+print(f"{5}시간에 {korean_wage*5}달러 벌었습니다.")
