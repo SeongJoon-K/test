7c7844eefb495a4f2959a7e3d7340e9986c5be70
hisocean <jobcho6320@naver.com>
2021-12-25 16:06:03 +0900

    christams day

---
+++ basic.py
@@ -0,0 +1,33 @@
+# # backjoon 10171
+# print("\\    /\\")
+# print(" )  ( \')")
+# print("(  /  )")
+# print(" \\(__)|")
+
+# # backjoon 10172
+# print("|\\_/|")
+# print("|q p|   /}")
+# print("( 0 )\"\"\"\\")
+# print("|\"^\"`    |")
+# print("||_/=\\\\__|")
+
+# backjoon a+b
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+# while True :
+#     A = int(input("숫자 A 입력"))
+#     if A > 0 :
+#         break
+
+# while True :
+#     B = int(input("숫자 B 입력"))
+#     if B < 10 :
+#         break
+# C = A+B
+
+# print("A+B = %d" %C)
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
