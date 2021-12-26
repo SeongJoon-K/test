85e406009fa859a1c09b9a85908df78448b69782
hisocean <jobcho6320@naver.com>
2021-12-26 15:25:04 +0900

    if pratice2

--- if.py
+++ if.py
@@ -22,12 +22,52 @@
 # else :
 #     print("You get an F.")
 
-i = 8
-while i <= 100 :
-    if i % 8 == 0 :
-        if i % 12 == 0 :
-            i += 8
-        else :
-            print(i)
-            i += 8
+# i = 8
+# while i <= 100 :
+#     if i % 8 == 0 :
+#         if i % 12 == 0 :
+#             i += 8
+#         else :
+#             print(i)
+#             i += 8
     
+
+# # 1000보다 작은 2,3 배수의 총합 구하기
+
+# sum = 0
+# i = 1
+# while i < 1000 :
+#     if i % 2 == 0 :
+#         sum += i
+#         i += 1
+#     elif i % 3 == 0 :
+#         sum += i
+#         i += 1
+#     else :
+#         i += 1
+# print(sum)
+
+# #if while 연습 120의 약수 구하기
+# # 정수 120의 약수
+
+# i = 1
+
+# while i <= 120 :
+#     if 120 % i == 0:
+#         print(i)
+#         i += 1
+#     else :
+#         i += 1
+
+# print("120의 약수는 총 16개입니다.")
+
+
+before_year = 0
+rate_plusmoney = 50000000
+wongm = 0
+
+while before_year < 28:
+    rate_plusmoney += rate_plusmoney * 0.12
+    before_year += 1
+
+print(rate_plusmoney)
