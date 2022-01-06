9bae945f88cdc874dd64e1df3a939366ece55c88
hisocean <jobcho6320@naver.com>
2022-01-06 15:10:06 +0900

    day1

--- list.py
+++ list.py
@@ -1,8 +1,27 @@
-num= [1,2,3,4,5,6]
-names = ["윤수","혜린","태호","영훈"]
+# numbers = []
 
-print(num[1] + num[0]) #인덱스는 0부터 시작됨
-# 리스트 슬라이싱
-print(num[:3])
-num[0] = 100
-print(num)
+# numbers.append(5)
+# print(numbers)
+
+
+# numbers.append(8)
+# print(numbers)
+
+
+# numbers.append(10)
+# print(numbers)
+
+# numbers.insert(0,0)
+# print(numbers)
+
+# 원소 빼기
+
+numbers = [1,2,3,4,5,6,7,8]
+
+del numbers[3]
+print(numbers)
+
+# 인덱스 4부터 마지막 값까지 삭제
+
+del numbers[4:]
+print(numbers)
