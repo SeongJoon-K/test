e64b06c5cf0f7ce66cc3e4b6104aa03455c87406
hisocean <jobcho6320@naver.com>
2022-01-07 14:59:13 +0900

    list pratice

--- 220107.py
+++ 220107.py
@@ -21,32 +21,61 @@
 
 
 
-# 원(￦)에서 달러($)로 바꿔주는 함수
-def krw_to_usd(won):
-    return won / 1000
+# # 원(￦)에서 달러($)로 바꿔주는 함수
+# def krw_to_usd(won):
+#     return won / 1000
 
-# 달러($)에서 엔(￥)로 바꿔주는 함수
-def usd_to_jpy(dollars):
-    return dollars * 125
+# # 달러($)에서 엔(￥)로 바꿔주는 함수
+# def usd_to_jpy(dollars):
+#     return dollars * 125
 
-# 원(￦)으로 각각 얼마인가요?
-amounts = [1000, 2000, 3000, 5000, 8000, 13000, 21000, 34000]
-print("한국 화폐: " + str(amounts))
+# # 원(￦)으로 각각 얼마인가요?
+# amounts = [1000, 2000, 3000, 5000, 8000, 13000, 21000, 34000]
+# print("한국 화폐: " + str(amounts))
  
-# amounts를 원(￦)에서 달러($)로 바꿔주기
-i = 0
-while i < len(amounts) :
-    amounts[i] = krw_to_usd(amounts[i])
+# # amounts를 원(￦)에서 달러($)로 바꿔주기
+# i = 0
+# while i < len(amounts) :
+#     amounts[i] = krw_to_usd(amounts[i])
+#     i += 1
+
+# print("미국 화폐: " + str(amounts))
+
+# # amounts를 달러($)에서 엔(￥)으로 바꿔주기
+
+# k = 0
+# while k < len(amounts):
+#     amounts[k] = usd_to_jpy(amounts[k])
+#     k += 1
+
+# # 엔(￥)으로 각각 얼마인가요?
+# print("일본 화폐: " + str(amounts))
+
+
+
+# 빈 리스트 만들기
+numbers = []
+
+# numbers에 자연수 1부터 10까지 추가
+i = 1
+while True :
+    numbers.append(i)
     i += 1
-
-print("미국 화폐: " + str(amounts))
-
-# amounts를 달러($)에서 엔(￥)으로 바꿔주기
-
+    if i == 11 :
+        break
+print(numbers)
+    
+# # numbers에서 홀수 제거
 k = 0
-while k < len(amounts):
-    amounts[k] = usd_to_jpy(amounts[k])
-    k += 1
+while k <= 9 :
+    numbers.remove(1+k)
+    k += 2
+print(numbers)
 
-# 엔(￥)으로 각각 얼마인가요?
-print("일본 화폐: " + str(amounts))
+# numbers의 인덱스 0 자리에 20이라는 값 삽입
+numbers.insert(0,20)
+print(numbers)
+
+# numbers를 정렬해서 출력
+numbers.sort()
+print(numbers)
