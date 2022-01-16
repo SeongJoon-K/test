0080457af4d8d572c73b8ed60b280cbc67384b33
hisocean <jobcho6320@naver.com>
2022-01-16 23:32:07 +0900

    116js

---
+++ objectPr1.js
@@ -0,0 +1,18 @@
+// 자바스크립트의 모든 것이 객체
+
+// 객체 생성 Property() key, value)
+// Property Name Caution 
+// 1. 첫 글자는 문자, _, $로 시작
+// 2. 띄어쓰기 x
+// 3. 하이픈 x
+
+console.log(typeof {
+    Name : '타운잇',
+    year : 2017,
+    Val : 'Flat',
+    Course: null,
+    objecto : {
+        Num1 : '오브젝트1',
+        Num2 : '오브젝트2'
+    }
+})
