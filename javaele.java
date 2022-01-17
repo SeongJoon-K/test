da003d9a5d64c0d8330c835e3a9f3ff5f1f6da6b
hisocean <jobcho6320@naver.com>
2022-01-17 16:21:53 +0900

    bootcampday1

---
+++ javaele.java
@@ -0,0 +1,15 @@
+public class javaele {
+    public static void main(String[] args) {
+
+    boolean myBoolean = true;
+    myBoolean = false;
+
+    int myInt = 123;
+    long myLong = 12345678910L;
+    double myDouble = 3.14;
+    System.out.println(myBoolean);
+    String myString = "plus" + ""
+    // 객체를 변수의 자료형으로 사용할 수 있음
+    }
+}
+// 
